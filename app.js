69c14b5329014f8b84fefc003ce58e331d01a89a
suad-alwajeeh <121978600+tactic-tech@users.noreply.github.com>
2023-03-10 19:25:01 +0300

    upload change

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ const port = process.env.PORT || 60000;
 //process.env.GOOGLE_API_KEY
 var server = app.listen(port, function () {
   console.log('Node listening on port 60000')
-})
-var users = [];
-var users_s = [];
+}) 
 var io = require('socket.io')(server, {
   cors: {
     methods: ["GET", "POST"],
@@ -17,17 +15,11 @@ var io = require('socket.io')(server, {
 
  io.on('connection', (socket) => {
     console.log('connection');
-    
-    //to create chat room
-    socket.on('create', function (data) {
-      console.log("create room" + data.room)
-      socket.join(data.room);
-      //send room data to resiver
-      socket.broadcast.emit('invite', data );
-    });
+     
     //reciver joinRoom 
-    socket.on('joinRoom', function (data) {
-      socket.join(data.room);
+    socket.on('notification', function (data) {
+      //socket.join(data.room);
+      
       console.log("room is " + data.room)
     });
     
