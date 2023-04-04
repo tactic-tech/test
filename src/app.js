9cf9135b33b68a85f7dbe52d170548742f61bbe9
suad-alwajeeh <121978600+tactic-tech@users.noreply.github.com>
2023-04-04 11:10:24 +0300

    socket.io test

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
-const express = require('express')  
- app = express()
+const express = require('express')
+app = express()
+const { v4: uuidV4 } = require('uuid') 
+const users = {};
+const socketToRoom = {};
+
+
 const port = process.env.PORT || 60000;
 //process.env.GOOGLE_API_KEY
 var server = app.listen(port, function () {
@@ -13,22 +18,35 @@ var io = require('socket.io')(server, {
   }
 });
 
- io.on('connection', (socket) => {
-    console.log('connection');
-    //spicific classroom
-    socket.on('classroomNotification', function (data) {
-      socket.broadcast.emit('classroom_'+data.id, data );
+io.on('connection', (socket) => {
+  console.log('connection');
+  //spicific classroom
+  socket.on('classroomNotification', function (data) {
+    socket.broadcast.emit('classroom_' + data.id, data);
 
-    });
-    
-    socket.on('notification', function (data) {
-      socket.broadcast.emit('notification_'+data.reciver_id, data );
-    });
-
-    
-     
-  
   });
-   
-  
-  
+
+  socket.on('notification', function (data) {
+    socket.broadcast.emit('notification_' + data.reciver_id, data);
+  });
+  socket.on("join-room", (data) => {
+    socket.join(data.roomId);
+    console.log(data.myname);
+    socket.to(data.roomId).emit("user-connected", data.id, data.myname);
+
+    socket.on("messagesend", (data) => {
+        console.log(data.message);
+        io.to(data.roomId).emit("createMessage", data.message);
+    });
+
+    socket.on("tellName", (data) => {
+        console.log(data.myname);
+        socket.to(data.roomId).emit("AddName", data.myname);
+    });
+
+    socket.on("disconnect", () => {
+        socket.to(data.roomId).emit("user-disconnected", data.id);
+    });
+});
+});
+
